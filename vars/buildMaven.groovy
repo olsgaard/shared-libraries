67b7d3d5213fcfd4e8db526b79953f686c095a7e
mads Olsgaard <mads@olsgaard.dk>
2017-12-06 14:47:55 +0100

    enclose in call()-function

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,1 +1,3 @@
-sh 'docker run -i --rm --name my-maven-project -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn install'
+def call() {
+	sh 'docker run -i --rm --name my-maven-project -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-8 mvn install'
+}
